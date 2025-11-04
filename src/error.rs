b04f9534204c1a68a55682d806d3925b493647fc
Szymanowski2D <Szymanowski2D@outlook.com>
2025-11-04 18:40:19 +0800

    feat: Expose WriteError

--- src/error.rs
+++ src/error.rs
@@ -1,3 +1,4 @@
-use crate::raft::config::type_config::{CheckIsLeaderError, RaftError};
+use crate::raft::config::type_config::{CheckIsLeaderError, ClientWriteError, RaftError};
 
 pub type ReadError = RaftError<CheckIsLeaderError>;
+pub type WriteError = RaftError<ClientWriteError>;
